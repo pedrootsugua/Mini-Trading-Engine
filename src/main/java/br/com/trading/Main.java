77960cd619ca168cd4b1f2bf571ebabe05e1bda4
pedrootsugua <pedro.zanele@gmail.com>
2026-01-15 01:21:38 -0300

    estrutura do projeto

---
+++ src/main/java/br/com/trading/Main.java
@@ -0,0 +1,7 @@
+package br.com.trading;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/br/com/trading/model/Side.java
@@ -0,0 +1,17 @@
+package br.com.trading.model;
+
+public class Side {
+
+    public enum side {
+        BUY,
+        SELL
+    }
+}
+
+
+
+
+
+
+
+
